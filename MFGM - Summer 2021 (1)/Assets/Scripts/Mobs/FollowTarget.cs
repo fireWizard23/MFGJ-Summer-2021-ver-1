5d7fb1e575d8313760848779251da2a089c87ccb
fireWizard23 <62689803+fireWizard23@users.noreply.github.com>
2021-07-01 13:52:14 +0800

    Commit ver 1.8
    Integrated NavMesh Plus successfully. Also installed Cinemachine and used it for the camera.

--- MFGM - Summer 2021 (1)/Assets/Scripts/Mobs/FollowTarget.cs
+++ MFGM - Summer 2021 (1)/Assets/Scripts/Mobs/FollowTarget.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class FollowTarget : MonoBehaviour
 {
@@ -9,23 +10,32 @@ public class FollowTarget : MonoBehaviour
     public NPC_SO myInfo;
 
     private Rigidbody2D myRigidbody;
+    private NavMeshAgent myAgent;
 
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        myAgent = GetComponent<NavMeshAgent>();
+
+        myAgent.updateRotation = false;
+        myAgent.updateUpAxis = false;
+        myAgent.updatePosition = false;
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector2 targetPos= target.position - transform.position;
+        myAgent.SetDestination(target.position);
+
+        Vector2 targetPos = target.position - transform.position;
         bool inDistance = targetPos.sqrMagnitude > myInfo.StopDistance * myInfo.StopDistance;
-        MyUtils.Print(targetPos.sqrMagnitude, myInfo.StopDistance * myInfo.StopDistance);
         if (inDistance)
         {
-            myRigidbody.velocity = targetPos.normalized * myInfo.MovementSpeed;
+            myRigidbody.velocity = myAgent.desiredVelocity.normalized;
+            myAgent.nextPosition = myRigidbody.position;
         }
-        else
+        else  
         {
             myRigidbody.velocity = Vector2.zero;
         }
